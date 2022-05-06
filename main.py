02715f42d3a3b5ec60839781e6fa5447561aea60
Aryan Bhardwaj <100504332+Aryan8445@users.noreply.github.com>
2022-05-06 15:17:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import requests
+from tkinter import *
 from pprint import pprint
+
+# Taking user input for the desired location
+location = input("Enter the desired location: ")
+
+root = Tk()
+root.title("Weathergo.com - Know Weather Anywhere")
+root.geometry("600x600")
 
 # API key for weather forecasting
 API_Key = "01351ad31924d80208cb67d9b98a9a5f"
 
-# Taking user input for the desired location
-location = input("Enter the location: ")
-
 # storing the weather data of the desired location
 weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid="
 
-# Combining both weather url and the API key to store the final result
-final_url = weather_url + API_Key
+try:
+    final_url = weather_url + API_Key
+    weather_data = requests.get(final_url).json()
+except Exception as A:
+    weather_data = "Error"
 
-# using reqests module to get the whether data and store the information accordingly
-weather_data = requests.get(final_url).json()
+w_lable = Label(root, highlightbackground="Red", text= weather_data)
+w_lable.pack()
 
-#Printing teh data in a readable format
-pprint(weather_data) 
+
+root.mainloop()
+ 
